7462406f3fcc4983c66c2605b0dcc6f5405ff367
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-17 00:05:40 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: 123, store: 'Kroger')
+Coupon.create(coupon_code: 321, store: 'Publix')
+Coupon.create(coupon_code: 456, store: 'Target')
